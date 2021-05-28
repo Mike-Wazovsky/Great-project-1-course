e9c309744876fa6fae0f1c2ce18b398c34467bc0
Fawentus <debase@bk.ru>
2021-05-28 12:56:05 -0400

    last version

--- src/view.cpp
+++ src/view.cpp
@@ -6,4 +6,4 @@ std::string View::start_game(std::string text) { return text; }
 
 std::string View::start_move(std::string move) { return move; }
 
-std::string View::end_game(std::string end) { return (end + "\nGame over"); }
+std::string View::end_game(std::string end) { return (end + "\nКонец игры"); }
